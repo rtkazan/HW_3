6cd2561ba0af849f3af0c284fd2a01f155ff2b1a
rtkazan <126402804+rtkazan@users.noreply.github.com>
2023-03-14 17:08:11 +0300

    Task19

---
+++ Task19.cs
@@ -0,0 +1,18 @@
+using System;  
+    class Program  
+    {  
+        static void Main(string[] args)  
+        {  
+          Console.Clear();
+          Console.Write("Введите пятизначное число: ");
+          int n = Convert.ToInt32(Console.ReadLine());
+          string nToString = Convert.ToString(n);
+          if (nToString[0] == nToString[4] && nToString[1] == nToString[3])
+          {
+              Console.WriteLine("да");
+          }    
+          else {
+              Console.WriteLine("нет");
+          }  
+        }  
+    }  

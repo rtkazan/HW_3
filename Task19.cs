1f7800987d7b07ed889ebfc1c75f55fcfd72180c
rtkazan <126402804+rtkazan@users.noreply.github.com>
2023-03-15 10:10:49 +0300

    Add files via upload

---
+++ Task19.cs
@@ -0,0 +1,23 @@
+using System;  
+    class Program  
+    {  
+        static void Main(string[] args)  
+        {  
+          Console.Clear();
+          Console.Write("Введите пятизначное число: ");
+          int n = Convert.ToInt32(Console.ReadLine());
+          string nToString = Convert.ToString(n);
+          while (nToString.Length > 4 && nToString.Length < 6)
+                 
+            if (nToString[0] == nToString[4] && nToString[1] == nToString[3])
+            {
+                Console.WriteLine("да");
+                break;
+            }
+            else Console.WriteLine("нет");
+          Console.WriteLine("не пятизначное число");
+              
+            
+        }
+    }
+

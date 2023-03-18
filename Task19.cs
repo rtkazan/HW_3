6462b4ce4c566e3dc086ae28707230ac6841b281
rtkazan <126402804+rtkazan@users.noreply.github.com>
2023-03-18 09:45:41 +0300

    Update Task19.cs

--- Task19.cs
+++ Task19.cs
@@ -1,14 +1,11 @@
-          Console.Clear();
+Console.Clear();
           Console.Write("Введите пятизначное число: ");
           int n = Convert.ToInt32(Console.ReadLine());
           string nToString = Convert.ToString(n);
-          while (nToString.Length > 4 && nToString.Length < 6)
-                 
-            if (nToString[0] == nToString[4] && nToString[1] == nToString[3])
-            {
-                Console.WriteLine("да");
-                break;
-            }
-            else Console.WriteLine("нет");
-          Console.WriteLine("не пятизначное число");
-            
+          if (nToString[0] == nToString[4] && nToString[1] == nToString[3])
+          {
+              Console.WriteLine("да");
+          }    
+          else {
+              Console.WriteLine("нет");
+          }
